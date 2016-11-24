353ca660c5db366129be2acc4c4c6807185474e9
Chris Stockton <chrisstocktonaz@gmail.com>
2016-11-24 14:39:39 -0700

    Update example.

--- example_test.go
+++ example_test.go
@@ -3,7 +3,6 @@ package iter_test
 import (
 	"errors"
 	"fmt"
-	"log"
 	"reflect"
 	"sort"
 	"strings"
@@ -11,7 +10,49 @@ import (
 	iter "github.com/cstockton/go-iter"
 )
 
+func Example() {
+
+	// Iter will walk any structured type, chans, slices, maps, structs.
+	type Package struct {
+		Name     string
+		Synopsis string
+
+		// Iter ascends into child structured types as well.
+		Parent *Package
+	}
+
+	// Iter will visit each key -> value pair. For structs the key will be the
+	// reflect.StructField, for maps the element key and the sequence integer for
+	// arrays and, slices.
+	pkgs := []Package{
+		{"io", "Package io provides basic interfaces to I/O primitives.", &Package{
+			"ioutil", "Package ioutil implements some I/O utility functions.", nil}},
+		{"hash", "Package hash provides interfaces for hash functions.", nil},
+		{"flag", "Package flag implements command-line flag parsing.", nil}}
+
+	fmt.Println("Packages:")
+	iter.Walk(pkgs, func(el iter.Pair) error {
+		fmt.Printf("  %v\n", el)
+		return nil
+	})
+
+	// Output:
+	// Packages:
+	//   Pair{(reflect.StructField) Name => io (string)}
+	//   Pair{(reflect.StructField) Synopsis => Package io p (string)}
+	//   Pair{(reflect.StructField) Name => ioutil (string)}
+	//   Pair{(reflect.StructField) Synopsis => Package iout (string)}
+	//   Pair{(reflect.StructField) Parent => <nil> (*iter_test.Package)}
+	//   Pair{(reflect.StructField) Name => hash (string)}
+	//   Pair{(reflect.StructField) Synopsis => Package hash (string)}
+	//   Pair{(reflect.StructField) Parent => <nil> (*iter_test.Package)}
+	//   Pair{(reflect.StructField) Name => flag (string)}
+	//   Pair{(reflect.StructField) Synopsis => Package flag (string)}
+	//   Pair{(reflect.StructField) Parent => <nil> (*iter_test.Package)}
+}
+
 func ExampleWalk() {
+
 	var res []string
 	m := map[int]string{1: "a", 2: "b", 3: "c"}
 
@@ -20,7 +61,7 @@ func ExampleWalk() {
 		return nil
 	})
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
 
 	sort.Strings(res) // for test determinism
@@ -35,6 +76,7 @@ func ExampleWalk() {
 }
 
 func ExampleWalk_errors() {
+
 	v := []interface{}{"a", "b", []string{"c", "d"}}
 	err := iter.Walk(v, func(el iter.Pair) error {
 		// check for errors
@@ -51,7 +93,7 @@ func ExampleWalk_errors() {
 		return nil
 	})
 	if err == nil {
-		log.Fatal(err)
+		fmt.Println("expected an error")
 	}
 
 	// Output:
@@ -60,6 +102,7 @@ func ExampleWalk_errors() {
 }
 
 func Example_recursion() {
+
 	type exampleWalk struct {
 		Head  string
 		Child *exampleWalk
@@ -90,7 +133,7 @@ func Example_recursion() {
 		return nil
 	})
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
 
 	// Output:
